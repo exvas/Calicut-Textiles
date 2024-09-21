0af9803242bb3ed2849e4b1f66fd7796423b5745
Jishnu-Epics <jishnu2016@gmail.com>
2024-09-21 12:43:53 +0530

    feat:sales invoice barcode scan changes

---
+++ calicut_textiles/calicut_textiles/events/sales_invoice.py
@@ -0,0 +1,72 @@
+import frappe
+from frappe import _
+from erpnext.stock.utils import _update_item_info
+from frappe.utils import *
+
+from typing import Dict, Optional
+
+BarcodeScanResult = Dict[str, Optional[str]]
+
+@frappe.whitelist()
+def scan_barcode(search_value: str) -> BarcodeScanResult:
+	def set_cache(data: BarcodeScanResult):
+		frappe.cache().set_value(f"erpnext:barcode_scan:{search_value}", data, expires_in_sec=120)
+
+	def get_cache() -> BarcodeScanResult | None:
+		if data := frappe.cache().get_value(f"erpnext:barcode_scan:{search_value}"):
+			return data
+
+	if scan_data := get_cache():
+		return scan_data
+
+	# search barcode no
+	barcode_data = frappe.db.get_value(
+		"Item Barcode",
+		{"barcode": search_value},
+		["barcode", "parent as item_code", "uom"],
+		as_dict=True,
+	)
+	if barcode_data:
+		_update_item_info(barcode_data)
+		set_cache(barcode_data)
+		return barcode_data
+
+	# search serial no
+	serial_no_data = frappe.db.get_value(
+		"Serial No",
+		search_value,
+		["name as serial_no", "item_code", "batch_no"],
+		as_dict=True,
+	)
+	if serial_no_data:
+		_update_item_info(serial_no_data)
+		set_cache(serial_no_data)
+		return serial_no_data
+
+	# search batch no
+	batch_no_data = frappe.db.get_value(
+		"Batch",
+		search_value,
+		["name as batch_no", "item as item_code", "batch_qty as qty"],
+		as_dict=True,
+	)
+	print(batch_no_data)
+	if batch_no_data:
+		if frappe.get_cached_value("Item", batch_no_data.item_code, "has_serial_no"):
+			frappe.throw(
+				_(
+					"Batch No {0} is linked with Item {1} which has serial no. Please scan serial no instead."
+				).format(search_value, batch_no_data.item_code)
+			)
+			
+        
+		_update_item_info(batch_no_data)
+		set_cache(batch_no_data)
+		return batch_no_data
+
+	return {}
+
+
+def validate_sales_person(doc,method):
+	if not doc.custom_sales_person:
+		frappe.throw(_("Please select a sales person"))
