05c1988e23eb05729599ddc6023bca545e3c3dac
Jishnu-Epics <jishnu2016@gmail.com>
2024-10-25 17:59:14 +0530

    fix:lop added

--- calicut_textiles/calicut_textiles/events/salary_slip.py
+++ calicut_textiles/calicut_textiles/events/salary_slip.py
@@ -1,18 +1,35 @@
 import frappe
 from frappe.utils import getdate, month_diff
-
+# from hrms.payroll.doctype.salary_slip.salary_slip import get_working_days_details
 
 
 def before_save(doc, method):
     if doc.employee and doc.start_date:
-        deducted_gross = calculate_deducted_gross(doc.employee, doc.start_date)
+        without_pay = frappe.db.get_list(
+            "Leave Application",
+            filters={
+                "employee": doc.employee,
+                "leave_type": "Leave Without Pay",
+                "status": "Approved",
+                "from_date": [">=", doc.start_date],
+                "to_date": ["<=", doc.end_date]
+            },
+            fields=["total_leave_days"]
+        )
+        
+        total_without_pay_days = sum(leave.total_leave_days for leave in without_pay) if without_pay else 0
+      
+        per_day = frappe.db.get_value("Salary Structure Assignment", {"employee": doc.employee}, "custom_leave_encashment_amount_per_day", order_by="creation desc")
+        
+        lop = per_day * total_without_pay_days
+       
+        deducted_gross = calculate_deducted_gross(doc.employee, doc.start_date) - lop
         
         if deducted_gross:
             doc.custom_deducted_gross = deducted_gross
-            
-            # doc.custom_deducted_per_day = deducted_gross / doc.payment_days
             doc.custom_deducted_basic = deducted_gross * 62.5 / 100
             doc.custom_deducted_da = deducted_gross * 40 / 100
+
 
 
 @frappe.whitelist()
