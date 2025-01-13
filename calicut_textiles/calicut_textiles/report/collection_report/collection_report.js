1ef917c656f2aabc9ecc4cb2c1cfc785255a698f
vijilavk <vijilavk46@gmail.com>
2025-01-13 11:21:14 +0530

    collectionreport

--- calicut_textiles/calicut_textiles/report/collection_report/collection_report.js
+++ calicut_textiles/calicut_textiles/report/collection_report/collection_report.js
@@ -3,6 +3,47 @@
 
 frappe.query_reports["Collection Report"] = {
 	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1,
+			"width":150,
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+			"width":150,
+		},
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer",
+			"width":150,
+		},
+		{
+			"fieldname": "invoice_id",
+			"label": __("Invoice-Id"),
+			"fieldtype": "Link",
+			"options": "Sales Invoice",
+			"width":150,
+		},
+		{
+			"fieldname": "mode_of_payment",
+			"label": __("Mode of Payment"),
+			"fieldtype": "Link",
+			"options": "Mode of Payment",
+			"width":150,
+			
+		},
+		
+		
+		
 
 	]
 };
