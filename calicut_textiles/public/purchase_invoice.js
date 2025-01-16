349108e268b4edd3fb069b3f14683e3e781ece9c
G-eethu <geethuk626@gmail.com>
2025-01-16 18:02:28 +0530

    feat: Modification in Purchase order, invoice and Receipt

---
+++ calicut_textiles/public/purchase_invoice.js
@@ -0,0 +1,16 @@
+frappe.ui.form.on("Purchase Invoice Item", {
+    custom_pcs: function(frm, cdt, cdn) {
+        update_qty(frm, cdt, cdn)
+    },
+    custom_net_qty: function(frm, cdt, cdn) {
+        update_qty(frm, cdt, cdn)
+    },
+
+
+});
+
+function update_qty(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+        row.qty = row.custom_pcs * row.custom_net_qty
+        frm.refresh_field("items");
+}
--- calicut_textiles/public/purchase_recipt.js
+++ calicut_textiles/public/purchase_recipt.js
@@ -39,6 +39,28 @@ frappe.ui.form.on('Purchase Receipt', {
                 });
             },__('Create')); 
         }
-    }
+    },
+    before_submit: function(frm) {
+        if (!frm.doc.lr_no) {
+            frappe.throw(__('LR No is mandatory before submitting the Purchase Receipt.'));
+        }
+    },
 
 });
+
+frappe.ui.form.on("Purchase Receipt Item", {
+    custom_pcs: function(frm, cdt, cdn) {
+        update_qty(frm, cdt, cdn)
+    },
+    custom_net_qty: function(frm, cdt, cdn) {
+        update_qty(frm, cdt, cdn)
+    },
+
+
+});
+
+function update_qty(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+        row.qty = row.custom_pcs * row.custom_net_qty
+        frm.refresh_field("items");
+}
