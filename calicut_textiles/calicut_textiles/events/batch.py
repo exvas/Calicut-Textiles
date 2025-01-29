a0e29ceae6fd8372523b7714da695a665efca903
Jishnu-Epics <jishnuvv2016@gmail.com>
2025-01-29 18:53:15 +0530

    fix:batch issue

--- calicut_textiles/calicut_textiles/events/batch.py
+++ calicut_textiles/calicut_textiles/events/batch.py
@@ -22,16 +22,17 @@ def update_batch_in_purchase_receipt(doc, method):
       
 
 def update_qty(doc, method):
-    for batch_entry in doc.entries:
-        if batch_entry.batch_no: 
-            pr_items = frappe.get_all(
-                'Purchase Receipt Item',
-                filters={'item_code': doc.item_code, 'qty': batch_entry.qty, 'parent': doc.voucher_no},
-                fields=['name', 'custom_net_qty']
-            )
-            
-            for pr_item in pr_items:
-                    print("fcgvhjkl",pr_item)
-                    frappe.db.set_value('Batch', batch_entry.batch_no, 'custom_qty', pr_item['custom_net_qty'])
-    frappe.db.commit()
+    if doc.voucher_type == 'Purchase Receipt':
+        for batch_entry in doc.entries:
+            if batch_entry.batch_no: 
+                pr_items = frappe.get_all(
+                    'Purchase Receipt Item',
+                    filters={'item_code': doc.item_code, 'qty': batch_entry.qty, 'parent': doc.voucher_no},
+                    fields=['name', 'custom_net_qty']
+                )
+                
+                for pr_item in pr_items:
+                        print("fcgvhjkl",pr_item)
+                        frappe.db.set_value('Batch', batch_entry.batch_no, 'custom_qty', pr_item['custom_net_qty'])
+        frappe.db.commit()
       
